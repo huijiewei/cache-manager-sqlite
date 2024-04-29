8e100187d14106aaef5ec19fc4113906c2bb5723
Huijie Wei <huijiewei@outlook.com>
2024-04-29 22:29:20 +0800

    feat: add CommonJS export and fix type export

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm'],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   external: ["better-sqlite3", "cache-manager"],
   dts: true,
   minify: true,
